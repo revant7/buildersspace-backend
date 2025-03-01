c7e9563da21bc6294f8aa33308dc1a01ea980b88
Revant <86226975+revant7@users.noreply.github.com>
2025-03-01 20:56:59 +0530

    Added Patch Requests

---
+++ home/utils.py
@@ -0,0 +1,58 @@
+import random, string
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import formataddr
+from django.template.loader import render_to_string
+
+
+def generate_random_password(length=12):
+    characters = string.ascii_letters + string.digits
+    password = "".join(random.choice(characters) for i in range(length))
+    return password
+
+
+def send_email(
+    subject,
+    message,
+    from_email,
+    from_name,
+    to_email,
+    html_template=None,
+    context=None,
+    smtp_server="smtp.gmail.com",
+    smtp_port=587,
+    smtp_username=None,
+    smtp_password=None,
+):
+
+    msg = MIMEMultipart("alternative")
+    msg["Subject"] = subject
+    msg["From"] = formataddr((from_name, from_email))
+    msg["To"] = to_email
+
+    part1 = MIMEText(message, "plain")
+    msg.attach(part1)
+
+    if html_template:
+        try:
+            html_content = (
+                render_to_string(html_template, context)
+                if context
+                else render_to_string(html_template)
+            )
+            part2 = MIMEText(html_content, "html")
+            msg.attach(part2)
+        except Exception as e:
+            print(f"Error rendering HTML template: {e}")
+
+    try:
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()
+            if smtp_username and smtp_password:
+                server.login(smtp_username, smtp_password)
+            server.sendmail(from_email, to_email, msg.as_string())
+        print(f"Email sent successfully to {to_email}")
+
+    except Exception as e:
+        print(f"Error sending email: {e}")
--- home/views.py
+++ home/views.py
@@ -6,9 +6,11 @@ from django.http import JsonResponse
 from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
-from . import models
+from . import models, utils
+
 
 # Create your views here.
+
 
 # User registration view
 
@@ -19,7 +21,7 @@ User = get_user_model()
 @permission_classes([permissions.AllowAny])
 def register_user(request):
     email = request.data.get("email")
-    password = request.data.get("password")
+    password = utils.generate_random_password()
     first_name = request.data.get("first_name")
     last_name = request.data.get("last_name", "")
     mobile_number = request.data.get("mobile_number")
@@ -75,6 +77,10 @@ def register_user(request):
         models.SocialLinks(user=user).save()
         models.ParticipantNotification(participant=participant).save()
 
+    if user.is_attendee:
+        attendee = models.Attendee(user=user)
+        attendee.save()
+
     return JsonResponse(
         {
             "message": "User registered successfully.",
@@ -128,10 +134,13 @@ def cast_vote(request):
     if user.is_attendee:
         participant = request.participant
         project = participant.project
+        attendee = user.attendee
+        already_voted = models.Vote.get(attendee=attendee)
+        if already_voted:
+            return JsonResponse({"status": "User Already Voted."})
         vote = models.Vote.objects.create(attendee=user.attendee, project=project)
-        vote_count, created = models.VoteCount.objects.get_or_create(
-            project=project, defaults={"count": 0}
-        )
+        vote.save()
+        vote_count = models.VoteCount.objects.get(project=project)
         vote_count.count += 1
         vote_count.save()
 
@@ -146,3 +155,153 @@ def do_like(request):
         )
         like_count.save()
         like_count.count += 1
+
+
+# patch user data
+
+
+@api_view(["PATCH"])
+@permission_classes([permissions.IsAuthenticated])
+def update_user(request):
+    try:
+        user = models.User.objects.get(email=request.user.email)
+    except User.DoesNotExist:
+        return Response({"error": "User not found."}, status=status.HTTP_404_NOT_FOUND)
+
+    first_name = request.data.get("first_name")
+    last_name = request.data.get("last_name")
+    mobile_number = request.data.get("mobile_number")
+    password = request.data.get("password")
+
+    if first_name is not None:
+        user.first_name = first_name
+    if last_name is not None:
+        user.last_name = last_name
+    if mobile_number is not None:
+        user.mobile_number = mobile_number
+    if password is not None:
+        user.set_password(password)
+
+    user.save()
+    return Response(
+        {"message": "User updated successfully."}, status=status.HTTP_200_OK
+    )
+
+
+# patch request to update participants data
+
+
+@api_view(["PATCH"])
+@permission_classes([permissions.IsAuthenticated])
+def update_participant(request):
+    try:
+        participant = models.Participant.objects.get(user=request.user)
+    except models.Participant.DoesNotExist:
+        return Response(
+            {"error": "Participant not found."}, status=status.HTTP_404_NOT_FOUND
+        )
+
+    house = request.data.get("house")
+    profile_picture = request.FILES.get("profile_picture")
+
+    if house is not None:
+        participant.house = house
+    if profile_picture is not None:
+        participant.profile_picture = profile_picture
+
+    participant.save()
+    return Response(
+        {"message": "Participant updated successfully."}, status=status.HTTP_200_OK
+    )
+
+
+# patch project data
+
+
+@api_view(["PATCH"])
+@permission_classes([permissions.IsAuthenticated])
+def update_project(request):
+    try:
+        project = models.Project.objects.get(participant__user=request.user)
+    except models.Project.DoesNotExist:
+        return Response(
+            {"error": "Project not found."}, status=status.HTTP_404_NOT_FOUND
+        )
+
+    project_idea_title = request.data.get("project_idea_title")
+    project_idea_description = request.data.get("project_idea_description")
+    project_experience = request.data.get("project_experience")
+    project_video_link = request.data.get("project_video_link")
+
+    if project_idea_title is not None:
+        project.project_idea_title = project_idea_title
+    if project_idea_description is not None:
+        project.project_idea_description = project_idea_description
+    if project_experience is not None:
+        project.project_experience = project_experience
+    if project_video_link is not None:
+        project.project_video_link = project_video_link
+
+    project.save()
+    return Response(
+        {"message": "Project updated successfully."}, status=status.HTTP_200_OK
+    )
+
+
+# update social links of user
+
+
+@api_view(["PATCH"])
+@permission_classes([permissions.IsAuthenticated])
+def update_social_links(request):
+    try:
+        social_links = models.SocialLinks.objects.get(user=request.user)
+    except models.SocialLinks.DoesNotExist:
+        return Response(
+            {"error": "Social links not found."}, status=status.HTTP_404_NOT_FOUND
+        )
+
+    instagram = request.data.get("instagram")
+    github = request.data.get("github")
+    twitter = request.data.get("twitter")
+    linkedin = request.data.get("linkedin")
+
+    if instagram is not None:
+        social_links.instagram = instagram
+    if github is not None:
+        social_links.github = github
+    if twitter is not None:
+        social_links.twitter = twitter
+    if linkedin is not None:
+        social_links.linkedin = linkedin
+
+    social_links.save()
+    return Response(
+        {"message": "Social links updated successfully."}, status=status.HTTP_200_OK
+    )
+
+
+# update participants notifications
+
+
+@api_view(["PATCH"])
+@permission_classes([permissions.IsAuthenticated])
+def update_participant_notification(request, notification_id):
+    try:
+        notification = models.ParticipantNotification.objects.get(
+            id=notification_id, participant__user=request.user
+        )
+    except models.ParticipantNotification.DoesNotExist:
+        return Response(
+            {"error": "Notification not found."}, status=status.HTTP_404_NOT_FOUND
+        )
+
+    is_read = request.data.get("is_read")
+
+    if is_read is not None:
+        notification.is_read = is_read
+
+    notification.save()
+    return Response(
+        {"message": "Notification updated successfully."}, status=status.HTTP_200_OK
+    )
