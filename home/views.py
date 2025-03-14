d556fe43023dca5724f20dd873de4eb5b5b6e69d
Revant <86226975+revant7@users.noreply.github.com>
2025-03-14 22:53:56 +0530

    updated views

--- home/views.py
+++ home/views.py
@@ -251,7 +251,7 @@ def do_like(request):
         like_count = models.LikeCount.objects.filter(project=project).update(
             count=F("count") + 1
         )
-        like_count.save()
+
         return JsonResponse({"status": "Attendee Successfully Liked."})
     if user.is_participant:
         if (
@@ -272,7 +272,6 @@ def do_like(request):
             count=F("count") + 1
         )
 
-        like_count.save()
         return JsonResponse({"status": "Participant Successfully Liked."})
 
 
